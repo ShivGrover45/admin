ad94d7833acbd51a3910026983406652a364e1be
Shiv Grover <127933032+ShivGrover45@users.noreply.github.com>
2024-05-24 16:52:48 +0530

    vote function updated

--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import get_object_or_404,render
 from django.http import Http404
 # Create your views here.
 #creating my first http response
-from django.http import HttpResponse
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
 
 from django.template import loader
 
+from django.db.models import F
 #def index(request):
 #    return HttpResponse("Hello Welcome to Python Server")
 def index(request):
@@ -29,5 +30,18 @@ def results(request,question_id):
     return HttpResponse(response,question_id)
 
 def vote(request,question_id):
-    return HttpResponse("you're voting on the question %s"%question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError,question.DoesNotExist):
+        return render(request,
+                      "polls/index.html",
+                      {
+                          "question":question,
+                                              "error_message":"You didn't select a question"                    })
+    else:
+        selected_choice.votes=F("votes")+1;
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question_id)))
+    
 
