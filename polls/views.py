6d4955e09085b7372ede6c099a7e09ff92e364ce
Shiv Grover <127933032+ShivGrover45@users.noreply.github.com>
2024-05-24 14:47:38 +0530

    view file updated

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404,render
+from django.http import Http404
 # Create your views here.
 #creating my first http response
 from django.http import HttpResponse
 
 from .models import Question
+
+from django.template import loader
+
 #def index(request):
 #    return HttpResponse("Hello Welcome to Python Server")
 def index(request):
     latest_questions_list=Question.objects.order_by("pub_date")[:5]
-    output=",".join([q.question_text for q in latest_questions_list])
-    return HttpResponse(output)
+    template=loader.get_template(".polls/index.html")
+    context={
+        "latest_question_list":latest_questions_list
+    }
+    #output=",".join([q.question_text for q in latest_questions_list])
+    return HttpResponse(template.render(context,request))
 
 def detail(request,question_id):
-    return HttpResponse("You're looking at question %s"%question_id)
+    try:
+        question=Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does not exist")
+    return render(request,'polls/detail.html',{"question":Question})
+    #return HttpResponse("You're looking at question %s"%question_id)
 
 def results(request,question_id):
     response="You are looking at the result of the questions uploaded %s"
