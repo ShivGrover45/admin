b22ef9da8a96b4a7eebb9dd87d7c7ecf87d33ae0
Shiv Grover <127933032+ShivGrover45@users.noreply.github.com>
2024-05-25 18:51:42 +0530

    url file updated

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_View(), name='index'),
-    path("<int:question_id>/",views.DetailView.as_View(),name="detail"),
-    path("<int:question_id>/results",views.ResultsView.as_View(),name="results"),
+    path('', views.IndexView.as_view(), name='index'),
+    path("<int:question_id>/",views.DetailView.as_view(),name="detail"),
+    path("<int:question_id>/results",views.ResultsView.as_view(),name="results"),
     path("<int:question_id>/votes",views.vote,name="vote"),
 ]
